6fa34bbb31b31c71fd45c522a18723ce535b0c08
will-hoefer <will.hoefer@gmail.com>
2020-07-17 10:36:01 -0400

    My branch is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { ViewTasksComponent } from './view-tasks/view-tasks.component';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { SelectTaskComponent } from './select-task/select-task.component';
+import { RouterModule } from '@angular/router';
+import { ViewService } from './services/view.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,18 @@ import { SelectTaskComponent } from './select-task/select-task.component';
     SelectTaskComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'view', component:ViewTasksComponent},
+      {path: 'create', component:CreateTaskComponent},
+      {path: 'select', component:SelectTaskComponent},
+      {path: '**', redirectTo:'view'},
+       ]),
+     ],
+  providers: [ViewService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse, HttpUrlEncodingCodec } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
+  constructor(private httpCli: HttpClient ) { }
 
-  constructor() { }
+  createTodos(todoForm): Observable<string>{
+    let url:string = 'http://ec2-54-175-137-78.compute-1.amazonaws.com:8080/todos'
+    const options= {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    }; 
+    return this.httpCli.post<string>(url, todoForm, options);
+  } 
 }
